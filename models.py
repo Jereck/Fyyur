d180c8a5c1175d014f3b7e3fef5cf9218c6bcda9
Jake Reck <jakereck@gmail.com>
2020-08-11 13:05:14 -0700

    Initial Commit

---
+++ models.py
@@ -0,0 +1,43 @@
+from app import db
+
+class Show(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+  start_time = db.Column(db.DateTime, nullable=False)
+
+class Venue(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  city = db.Column(db.String(120))
+  state = db.Column(db.String(120))
+  address = db.Column(db.String(120))
+  phone = db.Column(db.String(120))
+  image_link = db.Column(db.String(500))
+  facebook_link = db.Column(db.String(120))
+  genres = db.Column(db.ARRAY(db.String()))
+  website = db.Column(db.String(120))
+  seeking_talent = db.Column(db.Boolean, default=True)
+  seeking_description = db.Column(db.String())
+  shows = db.relationship('Show', backref='venue', lazy=True)
+
+  def __repr__(self):
+    return f'<Venue {self.id} - {self.name}>'
+
+class Artist(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  city = db.Column(db.String(120))
+  state = db.Column(db.String(120))
+  phone = db.Column(db.String(120))
+  genres = db.Column(db.String(120))
+  image_link = db.Column(db.String(500))
+  facebook_link = db.Column(db.String(120))
+  genres = db.Column(db.ARRAY(db.String()))
+  website = db.Column(db.String(120))
+  seeking_venue = db.Column(db.Boolean, default=True)
+  seeking_description = db.Column(db.String())
+  shows = db.relationship('Show', backref='artist', lazy=True)
+
+  def __repr__(self):
+    return f'<Artist {self.id} - {self.name}>'
